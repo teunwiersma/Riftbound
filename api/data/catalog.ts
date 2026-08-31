eb6bc634cfb2fcd108be05bdf41770e120b90ee5
Idris Dopico Peña <dopico1996@hotmail.com>
2026-08-31 23:56:39 +0200

    chore: formatting

--- api/data/catalog.ts
+++ api/data/catalog.ts
@@ -4,7 +4,7 @@ import { CardDTO } from "../types";
 const DEFAULT_PAGE_SIZE = 12;
 
 export const catalogData = async (page = 1, pageSize = DEFAULT_PAGE_SIZE) => {
-  const { data, error} = await getCatalogData(page, pageSize);
+  const { data, error } = await getCatalogData(page, pageSize);
 
   return {
     data,
