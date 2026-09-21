88121d8056a34c8188fe073864f6f57afb103bd1
teunwiersma <teunwiersma@gmail.com>
2026-09-21 16:53:08 +0200

    fix: remove redundant line in Card component for clarity

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -47,7 +47,6 @@ export default function Card({ data, className, controls, onClick }: Props) {
         <div className={style.image}>
           <Image
             className={isBattlefield ? style.battlefieldArt : undefined}
-            className={isBattlefield ? style.battlefieldArt : undefined}
             alt={data.name}
             src={data.art.imageURL ?? data.art.thumbnailURL}
             fill={true}
